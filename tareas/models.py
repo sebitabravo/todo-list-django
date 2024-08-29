0fa5311a0c99cfd74f7ca5f397c43ab6232f540b
sebitabravo <sebastianalejandrobravocampos@gmail.com>
2024-08-28 20:19:08 -0400

    first commit

---
+++ tareas/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=False)
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
